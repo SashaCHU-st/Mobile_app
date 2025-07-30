56bec513015d4ad1a6801f19d5c946466d2f3841
SashaCHU-st <alexandra.churina@gmail.com>
2025-07-30 09:09:35 +0300

    feat(friends): add check to prevent adding existing friend

--- server/src/controllers/AddFriends.ts
+++ server/src/controllers/AddFriends.ts
@@ -1,26 +1,32 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { addFriendBody } from "../types/types";
 import { pool } from "../db/db";
-import fr from "zod/v4/locales/fr.cjs";
 
 export async function addFriend(
   req: FastifyRequest<{ Body: addFriendBody }>,
   reply: FastifyReply
 ) {
-const userId = (req.user as { id: number }).id;
+  const userId = (req.user as { id: number }).id;
   const { friendsId } = req.body;
 
   try {
-    const addFriend = await pool.query(
-      `INSERT INTO friends (user_id, friends_id) VALUES ($1, $2)`,
-      [userId, friendsId]
+    const alreadyFriend = await pool.query(
+      `SELECT * FROM friends WHERE (user_id = $1 AND friends_id = $2) OR (user_id = $3 AND friends_id = $4)`,
+      [userId, friendsId, friendsId, userId]
     );
 
-    console.log("ADD friend=>", addFriend);
+    console.log("UUUU=>", alreadyFriend);
+    if (alreadyFriend.rowCount === 0) {
+      await pool.query(
+        `INSERT INTO friends (user_id, friends_id) VALUES ($1, $2)`,
+        [userId, friendsId]
+      );
+      return reply.code(201).send({ message: "All good" });
+    } else {
+      return reply.code(400).send({ message: "Alredy friends" });
+    }
 
-    return reply
-        .code(201)
-        .send({ message: "All good" });
+    // console.log("ADD friend=>", addFriend);
   } catch (err: any) {
     console.error("Database error:", err.message);
     return reply.code(500).send({ message: "Something went wrong" });

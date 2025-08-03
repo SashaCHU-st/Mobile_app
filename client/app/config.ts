e7f5231d5c5370c2f4f2d8dea68e77ab4fe9300c
SashaCHU-st <alexandra.churina@gmail.com>
2025-08-03 11:19:06 +0300

    (fix) change in congigurations

--- client/app/config.ts
+++
@@ -1,7 +0,0 @@
-// export const LOCAL_IP = "192.168.0.20";
-export const LOCAL_IP = "10.0.2.2";
-
-export const API_URL =
-  typeof navigator !== "undefined" && navigator.product === "ReactNative"
-    ? `http://${LOCAL_IP}:3001`
-    : "http://localhost:3001";

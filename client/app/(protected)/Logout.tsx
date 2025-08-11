b58fcac89a10f829a98348bf14383af2a32783c4
SashaCHU-st <alexandra.churina@gmail.com>
2025-08-11 12:56:41 +0300

    (feat) mace logout page straight away logout for user, no need to push button

--- client/app/(protected)/Logout.tsx
+++ client/app/(protected)/Logout.tsx
@@ -2,6 +2,8 @@ import { View, Text, Pressable, StyleSheet, Dimensions } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 import { useAuth } from "../context/Authcontext";
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback } from "react";
 const size = Dimensions.get("window").width * 0.1;
 
 const Logout = () => {
@@ -12,11 +14,14 @@ const Logout = () => {
     logout();
     router.replace("/");  
   };
+
+    useFocusEffect(
+      useCallback(() => {
+        handleLogout();
+      }, [])
+    );
   return (
-    <View style = {styles.container}>
-      <Pressable style={styles.button} onPress={handleLogout}>
-        <Text style={styles.text}>Logout</Text>
-      </Pressable>
+    <View>
     </View>
   );
 };

2e663caf880985b016cc7444cdbde67e20ac8ff5
SashaCHU-st <alexandra.churina@gmail.com>
2025-08-18 10:42:26 +0300

    deployed

--- client/app/components/SignUp.tsx
+++ client/app/components/SignUp.tsx
@@ -30,7 +30,7 @@ const SignUp = ({
 
   const handleSignUp = async () => {
     try {
-      const results = await fetch(`https://xxxx.onrender.com/signup`, {
+      const results = await fetch(`${API_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({

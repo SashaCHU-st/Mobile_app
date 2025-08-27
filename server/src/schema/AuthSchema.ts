6d319caef4f609c1b458b1c99d0f83b7846ec0ed
SashaCHU-st <alexandra.churina@gmail.com>
2025-08-27 10:20:22 +0300

    (fix) added extra check for names, password

--- server/src/schema/AuthSchema.ts
+++ server/src/schema/AuthSchema.ts
@@ -2,11 +2,15 @@ import { z } from "zod";
 
 export const SignUpSchema = z.object({
   email: z.string().email(),
-  name: z.string().min(1, "Name is required"),
-  password: z.string().min(4, "Paswword minimum 4 characters"),
+  name: z.string().min(1, "Name is required").refine((value) => value.trim().length > 0, 'Name cannot be only spaces')
+    .refine(
+      (value) => /^\p{L}+(?:[- ]\p{L}+)*$/u.test(value),
+      'Name can be only letters'
+    ),
+  password: z.string().min(4, "Paswword minimum 4 characters").max(40),
 });
 
 export const LoginSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(4, "Password minimum 4 characters"),
+  password: z.string().min(4, "Password minimum 4 characters").max(40),
 });
--- server/src/schema/ProfileSchema.ts
+++ server/src/schema/ProfileSchema.ts
@@ -1,8 +1,13 @@
 import { z } from "zod";
 
 export const ProfileSchema = z.object({
-  name: z.coerce.string(),
-  password: z.coerce.string(),
+  name: z.coerce.string().max(20)
+    .refine((value) => value.trim().length > 0, 'Name cannot be only spaces')
+    .refine(
+      (value) => /^\p{L}+(?:[- ]\p{L}+)*$/u.test(value),
+      'Name can be only letters'
+    ),
+  password: z.coerce.string().min(4, "Paswword minimum 4 characters"),
   image: z
     .string()
     .startsWith("data:image", { message: "Invalid image format" })
